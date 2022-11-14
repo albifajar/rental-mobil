4b57254773832202e11f43f5355b2fc63bddd83f
albifajar <albi@sawala.tech>
2022-11-14 12:01:15 +0700

    feat: acumulation

--- script.js
+++ script.js
@@ -1,45 +1,78 @@
-function crud() {
+const listmobil = (index = false) => {
+  let data = [
+    {
+      name: "Avanza",
+      harga: 20000,
+      id: 0,
+    },
+    {
+      name: "testing",
+      harga: 20000,
+      id: 1,
+    },
+  ];
+  if (data[index]) {
+    return data[index];
+  }
+  return data;
+};
+
+const convertRange = (date) => {
+  let rangeDate = date.split(" to ");
+  const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+  const firstDate = new Date(rangeDate[0]);
+  const secondDate = new Date(rangeDate[1]);
+
+  const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
+  return diffDays;
+};
+
+const crud = () => {
   return {
     addMode: true,
     id: "",
     form: {
       name: "",
-      mobil: "",
+      mobil: {},
       waktu: "",
+      total: 0,
     },
     students: [
       {
         name: "testing",
-        mobil: "testing",
+        mobil: 0,
         waktu: "",
+        total: 0,
       },
     ],
-    saveData() {
-      if (this.form.name.length && this.form.mobil.length) {
-        console.log(this.form);
-        this.students.push({
-          name: this.form.name,
-          mobil: this.form.mobil,
-          waktu: this.form.waktu,
-        });
-        this.resetForm();
-      }
-    },
     detail(student, index) {
       new bootstrap.Modal("#editData", {
         keyboard: false,
       }).show();
 
       this.form.name = student.name;
-      this.form.mobil = student.mobil;
+      this.form.mobil = student.mobil.id;
       this.form.waktu = student.waktu;
+      this.form.total = student.total;
       this.id = index;
+    },
+    saveData() {
+      if (this.form.name.length && this.form.mobil.length) {
+        console.log(this.form);
+        this.students.push({
+          name: this.form.name,
+          mobil: listmobil(this.form.mobil),
+          waktu: this.form.waktu,
+          total: this.form.total,
+        });
+        this.resetForm();
+      }
     },
     updateData() {
       if (this.form.name.length && this.form.mobil.length) {
         this.students.splice(this.id, 1, {
           name: this.form.name,
-          mobil: this.form.mobil,
+          mobil: listmobil(this.form.mobil),
         });
         this.resetForm();
       }
@@ -47,14 +80,11 @@ function crud() {
     deleteData(index) {
       this.students.splice(index, 1);
     },
-    cancelEdit() {
-      this.resetForm();
-    },
     resetForm() {
       this.form.name = "";
-      this.form.mobil = "";
+      this.form.mobil = {};
       this.form.waktu = "";
       this.addMode = true;
     },
   };
-}
+};
